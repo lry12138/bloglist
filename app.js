adc24f473adb874b5c450c0b8ab14fb73e4cd8da
R Li <lry.ox.uk@gmail.com>
2023-11-27 19:44:15 +0000

    ex 4.18-4.19

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.connect(config.mongoUrl)
 
@@ -18,6 +19,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users',usersRouter)
+app.use('/api/login',loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
